022df78f4a58be0e085aceaf3893b43e1e3d4f9e
Gunnish Mehta <gunnishmehta2003@gmail.com>
2023-12-26 16:35:02 +0530

    React Project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechStar</h1>
+            <p>@all rights reserved</p>
+        </div>
+        <div>
+            <h5>Follow us</h5>
+            <div>
+                <a href="https://youtube.com" target='blank'>Youtube</a>
+                <a href="https://linkedin.com" target='blank'>LinkedIn</a>
+                <a href="https://github.com" target='blank'>Github</a>
+            </div>
+        </div>
+        
+    </footer>
+  )
+}
+
+export default Footer
